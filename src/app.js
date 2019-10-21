07ad3c73efc7165651e97125ba31e41e1df3ffdb
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-21 21:09:47 +0100

    MVP completed

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+      todos: [
+        "Buy shopping",
+        "Clean bathroom",
+        "Car MOT"
+      ],
+      newItem: ""
+    },
+    methods: {
+      saveNewItem: function(){
+        this.todos.push(this.newItem);
+        this.newItem = ""
+      }
+    }
+  });
+})
